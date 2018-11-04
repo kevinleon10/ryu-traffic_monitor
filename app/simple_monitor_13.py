a8e3becbfd0b27a33e0c0e4ed24ea67b343c5462
kevinleon10 <lskevin97@hotmail.com>
2018-11-03 22:43:29 -0600

    Primer commit, contiene la parte de estadisticas 100% funcional. Hace falta bloquear flujos

---
+++ app/simple_monitor_13.py
@@ -0,0 +1,231 @@
+# Copyright (C) 2016 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from operator import attrgetter
+import simple_switch_13
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER
+from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.app.wsgi import ControllerBase
+from ryu.app.wsgi import Response
+from ryu.app.wsgi import route
+from ryu.app.wsgi import WSGIApplication
+from ryu.lib import dpid as dpid_lib
+from ryu.lib import hub
+import json
+
+simple_switch_instance_name = 'simple_switch_api_app'
+url = '/mactable/{dpid}'
+
+
+class SimpleMonitor13(simple_switch_13.SimpleSwitch13):
+    _CONTEXTS = {'wsgi': WSGIApplication}
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleMonitor13, self).__init__(*args, **kwargs)
+        self.datapaths = {}
+        self.monitor_thread = hub.spawn(self._monitor)
+        self.switches = {}
+        self.ev = None
+        self.ev2 = None
+        wsgi = kwargs['wsgi']
+        wsgi.register(SimpleSwitchController,
+                      {simple_switch_instance_name: self})
+
+    @set_ev_cls(ofp_event.EventOFPStateChange,
+                [MAIN_DISPATCHER, DEAD_DISPATCHER])
+    def _state_change_handler(self, ev):
+        datapath = ev.datapath
+        if ev.state == MAIN_DISPATCHER:
+            if datapath.id not in self.datapaths:
+                self.logger.debug('register datapath: %016x', datapath.id)
+                self.datapaths[datapath.id] = datapath
+        elif ev.state == DEAD_DISPATCHER:
+            if datapath.id in self.datapaths:
+                self.logger.debug('unregister datapath: %016x', datapath.id)
+                del self.datapaths[datapath.id]
+
+    def _monitor(self):
+        while True:
+            for dp in self.datapaths.values():
+                self._request_stats(dp)
+            hub.sleep(1)
+
+    def _request_stats(self, datapath):
+        self.logger.debug('send stats request: %016x', datapath.id)
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        req = parser.OFPFlowStatsRequest(datapath)
+        datapath.send_msg(req)
+
+        req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
+        datapath.send_msg(req)
+
+    @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
+    def _flow_stats_reply_handler(self, ev):
+        body = ev.msg.body
+
+        # self.logger.info('datapath         '
+        #                  'in-port  eth-dst           '
+        #                  'out-port packets  bytes')
+        # self.logger.info('---------------- '
+        #                  '-------- ----------------- '
+        #                  '-------- -------- --------')
+        # for stat in sorted([flow for flow in body if flow.priority == 1],
+        #                    key=lambda flow: (flow.match['in_port'],
+        #                                      flow.match['eth_dst'])):
+        #     self.logger.info('%016x %8x %17s %8x %8d %8d',
+        #                      ev.msg.datapath.id,
+        #                      stat.match['in_port'], stat.match['eth_dst'],
+        #                      stat.instructions[0].actions[0].port,
+        #                      stat.packet_count, stat.byte_count)
+        #self.logger.info('%s', json.dumps(ev.msg.to_jsondict(), ensure_ascii=True,
+        #                                  indent=3, sort_keys=True))
+        self.ev = ev
+
+    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
+    def _port_stats_reply_handler(self, ev):
+        body = ev.msg.body
+
+        # self.logger.info('datapath         port     '
+        #                  'rx-pkts  rx-bytes rx-error '
+        #                  'tx-pkts  tx-bytes tx-error')
+        # self.logger.info('---------------- -------- '
+        #                  '-------- -------- -------- '
+        #                  '-------- -------- --------')
+        # for stat in sorted(body, key=attrgetter('port_no')):
+        #     self.logger.info('%016x %8x %8d %8d %8d %8d %8d %8d',
+        #                      ev.msg.datapath.id, stat.port_no,
+        #                      stat.rx_packets, stat.rx_bytes, stat.rx_errors,
+        #                      stat.tx_packets, stat.tx_bytes, stat.tx_errors)
+        #self.logger.info('%s', json.dumps(ev.msg.to_jsondict(), ensure_ascii=True,
+        #                                  indent=3, sort_keys=True))
+        self.ev2 = ev
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        super(SimpleMonitor13, self).switch_features_handler(ev)
+        datapath = ev.msg.datapath
+        self.switches[datapath.id] = datapath
+        self.mac_to_port.setdefault(datapath.id, {})
+
+    def set_mac_to_port(self, dpid, entry):
+        mac_table = self.mac_to_port.setdefault(dpid, {})
+        datapath = self.switches.get(dpid)
+
+        entry_port = entry['port']
+        entry_mac = entry['mac']
+
+        if datapath is not None:
+            parser = datapath.ofproto_parser
+            if entry_port not in mac_table.values():
+
+                for mac, port in mac_table.items():
+                    # from known device to new device
+                    actions = [parser.OFPActionOutput(entry_port)]
+                    match = parser.OFPMatch(in_port=port, eth_dst=entry_mac)
+                    self.add_flow(datapath, 1, match, actions)
+
+                    # from new device to known device
+                    actions = [parser.OFPActionOutput(port)]
+                    match = parser.OFPMatch(in_port=entry_port, eth_dst=mac)
+                    self.add_flow(datapath, 1, match, actions)
+
+                mac_table.update({entry_mac: entry_port})
+        return mac_table
+
+    def drop_web_packets(self):
+        datapath = self.ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        match = parser.OFPMatch(eth_type=0x0800, ip_proto=6, tcp_dst=80)
+        instruction = [
+            parser.OFPInstructionActions(ofproto.OFPIT_CLEAR_ACTIONS, [])
+        ]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=1,
+                                    match=match, instructions=instruction)
+        datapath.send_msg(mod)
+
+class SimpleSwitchController(ControllerBase):
+
+    def __init__(self, req, link, data, **config):
+        super(SimpleSwitchController, self).__init__(req, link, data, **config)
+        self.simple_switch_app = data[simple_switch_instance_name]
+
+    @route('simpleswitch', url, methods=['GET'],
+           requirements={'dpid': dpid_lib.DPID_PATTERN})
+    def list_mac_table(self, req, **kwargs):
+
+        simple_switch = self.simple_switch_app
+        dpid = dpid_lib.str_to_dpid(kwargs['dpid'])
+
+        if dpid not in simple_switch.mac_to_port:
+            return Response(status=404)
+
+        mac_table = simple_switch.mac_to_port.get(dpid, {})
+        body = json.dumps(mac_table)
+        return Response(content_type='application/json', body=body)
+
+    @route('simpleswitch', '/flows',
+           methods=['GET'])
+    def flow(self, req, **kwargs):
+        simple_switch = self.simple_switch_app
+        if simple_switch.ev is not None:
+            return Response(content_type='application/json',
+                            body=json.dumps(simple_switch.ev.msg.to_jsondict(), ensure_ascii=True,
+                                            indent=3, sort_keys=True))
+        else:
+            return Response(status=503)
+
+    @route('simpleswitch', '/ports',
+           methods=['GET'])
+    def port(self, req, **kwargs):
+        simple_switch = self.simple_switch_app
+        if simple_switch.ev2 is not None:
+            return Response(content_type='application/json',
+                            body=json.dumps(simple_switch.ev2.msg.to_jsondict(), ensure_ascii=True,
+                                            indent=3, sort_keys=True))
+        else:
+            return Response(status=503)
+
+    @route('simpleswitch', '/web',
+           methods=['GET'])
+    def web(self, req, **kwargs):
+        simple_switch = self.simple_switch_app
+        simple_switch.drop_web_packets()
+        return Response(status=200)
+
+    @route('simpleswitch', url, methods=['PUT'],
+           requirements={'dpid': dpid_lib.DPID_PATTERN})
+    def put_mac_table(self, req, **kwargs):
+
+        simple_switch = self.simple_switch_app
+        dpid = dpid_lib.str_to_dpid(kwargs['dpid'])
+        try:
+            new_entry = req.json if req.body else {}
+        except ValueError:
+            raise Response(status=400)
+
+        if dpid not in simple_switch.mac_to_port:
+            return Response(status=404)
+
+        try:
+            mac_table = simple_switch.set_mac_to_port(dpid, new_entry)
+            body = json.dumps(mac_table)
+            return Response(content_type='application/json', body=body)
+        except Exception as e:
+            return Response(status=500)
---
+++ app/simple_switch_13.py
@@ -0,0 +1,157 @@
+# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+import array
+
+
+class SimpleSwitch13(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # install table-miss flow entry
+        #
+        # We specify NO BUFFER to max_len of the output action due to
+        # OVS bug. At this moment, if we specify a lesser number, e.g.,
+        # 128, OVS will send Packet-In with invalid buffer_id and
+        # truncated packet data. In that case, we cannot output packets
+        # correctly.  The bug has been fixed in OVS v2.1.0.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        # If you hit this you might want to increase
+        # the "miss_send_length" of your switch
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+        dst = eth.dst
+        src = eth.src
+
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        #self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            print("In port:")
+            print(in_port)
+            print("Out port:")
+            print(out_port)
+            paquet = packet.Packet(array.array('B', ev.msg.data))
+            hasEthernet = False
+            hasAssignedMatch = False
+            dst_ip = None
+            src_ip = None
+            for p in paquet.protocols:
+                print (p)
+                if p.protocol_name == 'tcp':
+                    if p.src_port == 80:
+                        hasAssignedMatch = True
+                        match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src, eth_type=0x0800,
+                                                ipv4_dst=dst_ip,
+                                                ipv4_src=src_ip, ip_proto=6, tcp_src=p.src_port)
+                    elif p.dst_port == 80:
+                        hasAssignedMatch = True
+                        match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src, eth_type=0x0800,
+                                                ipv4_dst=dst_ip,
+                                                ipv4_src=src_ip, ip_proto=6, tcp_dst=p.dst_port)
+
+                elif p.protocol_name == 'udp':
+                    if p.src_port == 53:
+                        match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src, udp_src=p.src_port)
+                    elif p.dst_port == 53:
+                        match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src, udp_dst=p.dst_port)
+
+                elif p.protocol_name == 'ethernet':
+                    hasEthernet = True
+
+                elif p.protocol_name == 'ipv4':
+                    dst_ip = p.dst
+                    src_ip = p.src
+
+            if hasEthernet and not hasAssignedMatch:
+                match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 2, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 2, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
